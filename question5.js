2f62ca6f8af4462c45ed2174f4bf04e554607de7
unknown <YY447@ccs.local>
2025-03-14 19:22:36 +0800

    Added question 5

--- question5.js
+++ question5.js
@@ -1,17 +1,16 @@
 import express from 'express'
 import { Sequelize, DataTypes } from 'sequelize';
 
-// Initialize Express app
+
 const app = express();
 const port = 3000;
 
-// Set up Sequelize connection to MySQL
 const sequelize = new Sequelize('mysql://root:@localhost:3306/midterm-exam', {
   dialect: 'mysql',
   logging: false 
 });
 
-// Test the database connection
+
 sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
@@ -20,7 +19,6 @@ sequelize.authenticate()
     console.error('Unable to connect to the database:', err);
   });
 
-// Define the User model
 const User = sequelize.define('User', {
   id: {
     type: DataTypes.INTEGER,
@@ -47,7 +45,7 @@ const User = sequelize.define('User', {
 
 sequelize.sync({ force: false })
 
-// Define the /users route
+
 app.get('/users', async (req, res) => {
   try {
     // Fetch all users from the database
@@ -58,7 +56,7 @@ app.get('/users', async (req, res) => {
   }
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
